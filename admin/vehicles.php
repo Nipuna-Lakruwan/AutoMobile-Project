e8d438a43f35e88ea61c5ecea94bc4ad6c7b5f8f
Nipuna Lakruwan <laknipsa@gmail.com>
2024-10-28 21:15:26 +0530

    Refactor vehicles.php: Include navbar and update pagination controls

--- admin/vehicles.php
+++ admin/vehicles.php
@@ -23,82 +23,7 @@
     <!-- Main Content -->
     <div class="content">
       <!-- Navbar -->
-      <nav>
-        <i class='bx bx-menu'></i>
-        <form action="#">
-          <div class="form-input">
-            <input type="search" placeholder="Search...">
-            <button class="search-btn" type="submit"><i class='bx bx-search'></i></button>
-          </div>
-        </form>
-        <input type="checkbox" id="theme-toggle" hidden>
-        <label for="theme-toggle" class="theme-toggle"></label>
-        <a href="#" class="notif">
-          <i class='bx bx-bell'></i>
-          <span class="count">12</span>
-        </a>
-        <!--<a href="#" class="profile">
-                        <img src="images/profile.JPG">
-                    </a> -->
-        <div class="profile-dropdown">
-          <div onclick="toggle()" class="profile-dropdown-btn">
-            <div class="profile-img">
-              <i class="fa-solid fa-circle"></i>
-            </div>
-
-            <span>Nipuna
-              <i class="fa-solid fa-angle-down"></i>
-            </span>
-          </div>
-
-          <ul class="profile-dropdown-list">
-            <li class="profile-dropdown-list-item">
-              <a href="profile.html">
-                <i class="fa-regular fa-user"></i>
-                Edit Profile
-              </a>
-            </li>
-
-            <li class="profile-dropdown-list-item">
-              <a href="#">
-                <i class="fa-regular fa-envelope"></i>
-                Inbox
-              </a>
-            </li>
-
-            <li class="profile-dropdown-list-item">
-              <a href="#">
-                <i class="fa-solid fa-chart-line"></i>
-                Analytics
-              </a>
-            </li>
-
-            <li class="profile-dropdown-list-item">
-              <a href="#">
-                <i class="fa-solid fa-sliders"></i>
-                Settings
-              </a>
-            </li>
-
-            <li class="profile-dropdown-list-item">
-              <a href="#">
-                <i class="fa-regular fa-circle-question"></i>
-                Help & Support
-              </a>
-            </li>
-            <hr />
-
-            <li class="profile-dropdown-list-item">
-              <a href="#">
-                <i class="fa-solid fa-arrow-right-from-bracket"></i>
-                Log out
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <!-- End of Navbar -->
+      <?php include 'includes/navbar.php'; ?>
 
       <main>
         <div class="header">
@@ -207,26 +132,26 @@
               <!-- Add more rows as needed -->
             </tbody>
           </table>
-          <div class="pagination">
-            <span>Showing 1 to 1 of 1 total records</span>
-            <select id="rowsPerPage">
-              <option value="100">100</option>
-              <!-- Add more options as needed -->
-            </select>
-            <div class="pagination-controls">
-              <button>&laquo;</button>
-              <button>1</button>
-              <button>&raquo;</button>
-            </div>
+        </div>
+        <div class="pagination">
+          <span>Showing 1 to 1 of 1 total records</span>
+          <select id="rowsPerPage">
+            <option value="100">100</option>
+            <!-- Add more options as needed -->
+          </select>
+          <div class="pagination-controls">
+            <button>&laquo;</button>
+            <button>1</button>
+            <button>&raquo;</button>
           </div>
         </div>
-      </main>
     </div>
-  </div>
+    </main>
+  </div><br><br>
   <?php include 'includes/footer.php'; ?> <!-- Include the footer -->
-  
+
   <script src="/AutoMobile Project/admin/assets/js/index.js"></script>
-  <script src="/AutoMobile Project/admin/assets/js/meeting.js"></script>
+  <script src="/AutoMobile Project/admin/assets/js/vehicle.js"></script>
 </body>
 
 </html>
