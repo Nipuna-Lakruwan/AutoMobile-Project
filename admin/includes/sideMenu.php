8a5c190deb3a0fa16619fa44923f68db6d68f081
Nipuna Lakruwan <laknipsa@gmail.com>
2024-10-28 16:13:44 +0530

    Refactor sideMenu.php: Update menu item URLs

--- admin/includes/sideMenu.php
+++ admin/includes/sideMenu.php
@@ -1,16 +1,16 @@
 <ul class="side-menu">
-  <li class="active"><a href="../pages/dashboard.html"><i class='bx bxs-dashboard'></i>Dashboard</a></li>
-  <li><a href="../pages/vehicles.html"><i class='bx bx-car' ></i>Vehicles</a></li>
-  <li><a href="../pages/bookings.html"><i class='bx bx-calendar'></i>Bookings</a></li>
-  <li><a href="../pages/inspections.html"><i class='bx bxs-car-mechanic'></i>Inspections</a></li>
-  <li><a href="../pages/inventory.html"><i class='bx bx-basket' ></i>Inventory</a></li>
-  <li><a href="../pages/invoices.html"><i class='bx bxs-report' ></i>Invoices</a></li>
-  <li><a href="../pages/quotation.html"><i class='bx bx-checkbox-checked' ></i>Quotations</a></li>
-  <li><a href="../pages/sales.html"><i class='bx bx-cart' ></i>Sales</a></li>
-  <li><a href="../pages/purchasing.html"><i class='bx bx-cart-add' ></i>Purchasing</a></li>
-  <li><a href="../pages/cashBank.html"><i class='bx bxs-bank' ></i>Cash/Bank</a></li>
-  <li><a href="../pages/financial.html"><i class='bx bx-briefcase' ></i>Financial</a></li>
-  <li><a href="../pages/humanResource.html"><i class='bx bx-body' ></i>Human Resource</a></li>
-  <li><a href="../pages/reports.html"><i class='bx bx-circle' ></i>Reports</a></li>
-  <li><a href="../pages/administrtion.html"><i class='bx bx-cog'></i>Administration</a></li>
+  <li class="active"><a href="../index.php"><i class='bx bxs-dashboard'></i>Dashboard</a></li>
+  <li><a href="../vehicles.php"><i class='bx bx-car' ></i>Vehicles</a></li>
+  <li><a href="../bookings.php"><i class='bx bx-calendar'></i>Bookings</a></li>
+  <li><a href="../inspections.php"><i class='bx bxs-car-mechanic'></i>Inspections</a></li>
+  <li><a href="../inventory.php"><i class='bx bx-basket' ></i>Inventory</a></li>
+  <li><a href="../invoices.php"><i class='bx bxs-report' ></i>Invoices</a></li>
+  <li><a href="../quotation.php"><i class='bx bx-checkbox-checked' ></i>Quotations</a></li>
+  <li><a href="../sales.php"><i class='bx bx-cart' ></i>Sales</a></li>
+  <li><a href="../purchasing.php"><i class='bx bx-cart-add' ></i>Purchasing</a></li>
+  <li><a href="../cashBank.php"><i class='bx bxs-bank' ></i>Cash/Bank</a></li>
+  <li><a href="../financial.php"><i class='bx bx-briefcase' ></i>Financial</a></li>
+  <li><a href="../humanResource.php"><i class='bx bx-body' ></i>Human Resource</a></li>
+  <li><a href="../reports.php"><i class='bx bx-circle' ></i>Reports</a></li>
+  <li><a href="../administrtion.php"><i class='bx bx-cog'></i>Administration</a></li>
 </ul>
