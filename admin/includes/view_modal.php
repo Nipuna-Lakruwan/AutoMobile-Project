f88d9934f69faf2ea2ecd1cc49582df942c35763
Nipuna Lakruwan <laknipsa@gmail.com>
2025-01-16 00:18:40 +0530

    Add universal modal component: create HTML structure for dynamic content display and action button

---
+++ admin/includes/view_modal.php
@@ -0,0 +1,13 @@
+<div id="universalModal" class="modal">
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <h2 id="modalTitle"></h2>
+    <div id="modalBody">
+      <!-- Dynamic content will be loaded here -->
+    </div>
+    <div id="modalFooter">
+      <!-- Optional footer content -->
+      <button class="btn-primary" id="modalActionBtn">Save Changes</button>
+    </div>
+  </div>
+</div>
