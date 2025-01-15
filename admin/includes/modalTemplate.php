ba68297c80094a7ad74f741c9ab15fe82b308aec
Nipuna Lakruwan <laknipsa@gmail.com>
2025-01-16 00:18:54 +0530

    Add generic modal template: create HTML structure for dynamic content and action button

---
+++ admin/includes/modalTemplate.php
@@ -0,0 +1,10 @@
+<div id="genericModal" class="modal">
+    <div class="modal-content">
+        <span class="close-button" id="closeGenericModal">&times;</span>
+        <h2 id="modalTitle">Modal Title</h2>
+        <div id="modalBody">
+            <!-- Dynamic content will be injected here -->
+        </div>
+        <button id="modalActionButton">Action</button>
+    </div>
+</div>
