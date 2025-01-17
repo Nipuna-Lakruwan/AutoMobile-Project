bb2aac733b2d37c80ef7a5be9f24dd6df9315ffc
Nipuna Lakruwan <laknipsa@gmail.com>
2025-01-17 17:32:35 +0530

    Refactor service history fetching: update SQL query to join with officer table and streamline parameter binding

--- Employee/fetch_service_history.php
+++ Employee/fetch_service_history.php
@@ -5,44 +5,39 @@ $vehicleType = isset($_POST['vehicleType']) ? $_POST['vehicleType'] : '';
 $mechanic = isset($_POST['mechanic']) ? $_POST['mechanic'] : '';
 $date = isset($_POST['date']) ? $_POST['date'] : '';
 
-$sql = "SELECT sr.service_record_id, v.license_no, v.category, m.name as mechanic_name, c.cus_id, s.service_type, sr.date as service_date, sr.amount as total_price 
+$sql = "SELECT sr.service_record_id, v.license_no, v.category, CONCAT(o.fname, ' ', o.lname) as mechanic_name, c.cus_Id as cus_id, s.service_type, sr.date as service_date, sr.amount as total_price 
         FROM servicerecord sr 
         JOIN vehicle v ON sr.vehicle_id = v.vehicle_id 
-        JOIN mechanic m ON sr.officer_id = m.mechanic_id 
-        JOIN customer c ON v.cus_id = c.cus_id 
+        JOIN officer o ON sr.officer_id = o.officer_id 
+        JOIN customer c ON v.cus_id = c.cus_Id 
         JOIN service s ON sr.service_id = s.service_id 
         WHERE 1=1";
 
+$params = [];
+$types = '';
+
 if (!empty($vehicleType)) {
     $sql .= " AND v.category = ?";
+    $params[] = $vehicleType;
+    $types .= 's';
 }
+
 if (!empty($mechanic)) {
-    $sql .= " AND m.name = ?";
+    $sql .= " AND CONCAT(o.fname, ' ', o.lname) = ?";
+    $params[] = $mechanic;
+    $types .= 's';
 }
+
 if (!empty($date)) {
     $sql .= " AND sr.date = ?";
+    $params[] = $date;
+    $types .= 's';
 }
 
 $stmt = $conn->prepare($sql);
 
-$bindTypes = '';
-$bindValues = [];
-
-if (!empty($vehicleType)) {
-    $bindTypes .= 's';
-    $bindValues[] = $vehicleType;
-}
-if (!empty($mechanic)) {
-    $bindTypes .= 's';
-    $bindValues[] = $mechanic;
-}
-if (!empty($date)) {
-    $bindTypes .= 's';
-    $bindValues[] = $date;
-}
-
-if (!empty($bindTypes)) {
-    $stmt->bind_param($bindTypes, ...$bindValues);
+if (!empty($params)) {
+    $stmt->bind_param($types, ...$params);
 }
 
 $stmt->execute();
